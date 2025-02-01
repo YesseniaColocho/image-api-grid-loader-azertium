f34bb790acb4f581e2f51d3b14a012996bc894ff
YesseniaColocho <Colocho.yessenia@gmail.com>
2025-02-01 13:00:21 +0100

    soft loading for images

--- components/img-square.vue
+++ components/img-square.vue
@@ -2,7 +2,7 @@
   <Transition>
     <section v-if="showImage" class="image" @click="hideImg">
       <!--img :src="props.imageProp.url"/-->
-      <img :src="newURL(props.imageProp.id)" />
+      <Transition><img v-if="tagImage" :src="tagImage" /></Transition>
     </section>
   </Transition>
 </template>
@@ -12,10 +12,20 @@ import { newURL } from "#imports";
 const props = defineProps(["imageProp"]);
 
 let showImage = ref(true);
+const tagImage = ref("");
 
 function hideImg() {
   showImage.value = false;
 }
+
+//Precarga la imagen en JS y luego la muestra en template una vez cargada
+onMounted(() => {
+  const imgContainer = new Image();
+  imgContainer.src = newURL(props.imageProp.id);
+  imgContainer.onload = () => {
+    tagImage.value = imgContainer.src;
+  };
+});
 </script>
 
 <style scoped lang="scss">
@@ -34,9 +44,6 @@ function hideImg() {
   }
 }
 
-
-
-
 .v-enter-active,
 .v-leave-active {
   transition: all 0.5s ease;
@@ -46,7 +53,4 @@ function hideImg() {
 .v-leave-to {
   opacity: 0;
 }
-
-
-
 </style>
