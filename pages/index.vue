35b8c26641cb2d2ca7df9c3f96bffbf0787e12f7
YesseniaColocho <Colocho.yessenia@gmail.com>
2025-02-02 18:09:28 +0100

    Title page

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <TopBar></TopBar>
   <main>
     <ImgGrid :images="imagesStore.visibleImages"></ImgGrid>
-    <infinite-loading  @infinite="imagesStore.addImages()" />
+    <infinite-loading @infinite="imagesStore.addImages()" />
   </main>
 </template>
 
@@ -13,13 +13,18 @@ const imagesStore = useImagesStore();
 onMounted(async () => {
   await imagesStore.getImages();
 });
+
+useHead({
+  title: "Prueba Técnica Azertium",
+  meta: [{ name: "description", content: "Por Yessenia Colocho" }],
+});
 </script>
 
 <style scoped lang="scss">
 main {
   margin: 0 20px;
 }
-:deep(.v3-infinite-loading > div){
+:deep(.v3-infinite-loading > div) {
   display: flex;
   justify-content: center;
   padding-top: 10px;
